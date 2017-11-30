03a3315ff18f3e2706776a0ff31b1cf6b7e5da97
dan87134 <dsullivan@danal.com>
2017-11-30 15:20:05 -0500

    Both hard coded qtls_walk_outline and configurable qtls_walk are working

--- R/walk.R
+++ R/walk.R
@@ -10,22 +10,80 @@ next_level <- function(level) {
 	stringr::str_c(level, ".1")
 }
 
-qtls_walk <- function(quosure, qtbl = tibble::tibble(), level = "1", pass = 1) {
-	message("r1")
+qtls_walk_outline <- function(quosure, qtbl = tibble::tibble(), slevel = "1") {
+	class(qtbl) <- c("qtls_outline", class(qtbl))
 	head <- rlang::lang_head(quosure)
 	tail <- rlang::lang_tail(quosure)
-	qtbl <- dplyr::bind_rows(qtbl, data.frame(quosure = rlang::expr_label(head), id = level, stringsAsFactors = FALSE))
-	pass <- pass + 1
-	message(level)
-	slevel <- stringr::str_c(level, ".1")
+	qtbl <- dplyr::bind_rows(qtbl, data.frame(outline = slevel,
+																						expr = rlang::expr_label(head),
+																						stringsAsFactors = FALSE))
+	slevel <- stringr::str_c(slevel, ".1")
 	for (index in 1:length(tail)) {
 		item <- tail[[index]]
 		if (rlang::is_lang(item)) {
-		  qtbl <- qtls_walk(rlang::quo(!! item), qtbl, slevel)
+		  qtbl <- qtls_walk_outline(rlang::quo(!! item), qtbl, slevel)
 		} else {
-			qtbl <- dplyr::bind_rows(qtbl, data.frame(quosure = rlang::expr_label(item), id = slevel, stringsAsFactors = FALSE))
+			qtbl <- dplyr::bind_rows(qtbl, data.frame( outline = slevel,
+															 expr = rlang::expr_label(item),
+															 stringsAsFactors = FALSE))
 		}
 		slevel <- next_sibling(slevel)
 	}
 	qtbl
+}
+
+
+
+
+qtls_outline_context <- function() {
+
+	outline <- new.env()
+	outline$next_sibling <- function(level) {
+		re <- "\\d+$"
+		s <- as.integer(stringr::str_extract(level, re)) + 1L;
+		stringr::str_replace(level, re, as.character(s))
+	}
+	outline$tbl <- tibble::tibble()
+	class(outline$tbl) <- c("qtls_outline", "qtls_tbl", class(outline$tbl))
+	outline$head_processing <- function(head) {outline$tbl <-
+		dplyr::bind_rows(outline$tbl, data.frame(outline = outline$slevel,
+																			expr = rlang::expr_label(head),
+																			stringsAsFactors = FALSE))
+		outline$slevel <- stringr::str_c(outline$slevel, ".1")
+	}
+	outline$expr_processing <- function(expr) {
+		outline$tbl <- dplyr::bind_rows(outline$tbl, data.frame( outline = outline$slevel,
+																							 expr = rlang::expr_label(expr),
+																							 stringsAsFactors = FALSE))}
+	outline$lang_processing <- function(item) {
+		save <- outline$slevel
+		outline$tbl <- qtls_walk(rlang::quo(!! item), outline)
+		outline$slevel <- save
+	}
+	outline$post_loop <- function() {
+	}
+	outline$loop_start <- function() {
+
+	}
+	outline$loop_end <- function() {
+		outline$slevel <-  next_sibling(outline$slevel)
+	}
+	outline$slevel <- "1"
+	outline
+}
+
+qtls_walk <- function(quosure, context) {
+	head <- rlang::lang_head(quosure)
+	tail <- rlang::lang_tail(quosure)
+	context$head_processing(head)
+	for (index in 1:length(tail)) {
+		item <- tail[[index]]
+		if (rlang::is_lang(item)) {
+			context$lang_processing(item)
+		} else {
+			context$expr_processing(item)
+		}
+		context$loop_end()
+	}
+	context$tbl
 }
--- scratch/scratch1.R
+++ scratch/scratch1.R
@@ -1,8 +1,34 @@
 # scratch
 
 q <- rlang::quo(a * b + c * d)
-l <- qtls_walk(q)
+l <- qtls_walk_outline(q)
+l1 <- qtls_walk(q, qtls_outline_context())
+
+
 typeof(l)
 l[[1]]
 str(l)
 as.integer("10")
+
+
+
+suppressPackageStartupMessages(library(DiagrammeR))
+a_graph <-
+	create_graph(
+		directed = FALSE
+	) %>%
+	add_node(label = "+") %>%
+	add_node(label = "b") %>%
+	add_node(label = "c") %>%
+	add_edge(
+		from = 1,
+		to = 2,
+		rel = "interacted_with"  ) %>%
+	add_edge(from = 1, to = 3)
+set_node_attrs(a_graph, node_attr = color, values = "red")
+a_graph %>% render_graph(layout = "tree")
+
+remove.packages(DiagrammeR)
+
+set_node_attrs
+
