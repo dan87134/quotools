a7fcc10857f9a8e51dec55d33f8735db285e0826
dan87134 <dsullivan@danal.com>
2017-12-08 11:27:05 -0500

    tree traversal via cdr seems to work

--- scratch/modquoscr.R
+++ scratch/modquoscr.R
@@ -1,6 +1,98 @@
 # mod quosure scratch
 
 
+qa <- rlang::quo(a)
+qtls_what_is_it(qa)
+ea <- rlang::get_expr(qa)
+qtls_what_is_it(ea)
+
+q1 <- rlang::quo(1L)
+e1 <- rlang::get_expr(q1)
+qtls_what_is_it(e1)
+
+q2 <- rlang::quo("DFSDF")
+e2 <- rlang::get_expr(q2)
+qtls_what_is_it(e2)
+
+q3 <- rlang::quo(a + b)
+e3 <- rlang::get_expr(q3)
+qtls_what_is_it(e3)
+
+q4 <- rlang::quo(a + b + c)
+e4 <- rlang::get_expr(q4)
+qtls_what_is_it(e4)
+
+
+
+walk_cons <- function(q, level = 1, pass = 1) {
+	print(glue::glue("pass: {pass}  ------------------------------"))
+	print(glue::glue("q{level}: {qtls_what_is_it(q)}"))
+	if (level > 5)
+		return()
+	if (!rlang::is_node(q)) {
+		if(rlang::is_formula(q)) {
+		e <- rlang::f_rhs(q)
+		} else {
+			e <- rlang::get_expr(q)
+		}
+		print(glue::glue("e{level}: {e}"))
+		print(glue::glue("e{level}: {qtls_what_is_it(e)}"))
+		cdr <- rlang::node_cdr(e)
+		print(glue::glue("cdr {level}: {cdr}"))
+		print(glue::glue("cdrw{level} {length(cdr)}: {qtls_what_is_it(cdr)}"))
+		for(index in 1:length(cdr)) {
+			print(glue::glue("cd_child {index} {qtls_what_is_it(cdr[[index]])}"))
+		}
+		car <- rlang::node_car(e)
+		print(glue::glue("car{level}: {qtls_what_is_it(car)}"))
+		for(index in 1:length(cdr)) {
+			if(rlang::is_lang(cdr[[index]])) {
+				pass <- pass + 1
+			walk_cons(cdr[[index]], level + 1, pass)
+			}
+		}
+	}
+}
+
+
+q <- rlang::quo(a + b * c + d)
+walk_cons(q)
+
+
+p <- rlang::parse_quosure("a + b * c + d")
+
+qtls_what_is_it(p)
+
+
+rlang::get_expr
+
+node_walk <- function(.x, .f, ...) {
+	cur <- .x
+	while (!is.null(cur)) {
+		.f(cur, ...)
+		cur <- rlang::node_cdr(cur)
+	}
+	NULL
+}
+
+
+
+rlang::node_car
+
+
+rlang::lang_tail
+
+q <- rlang::quo(a + b * c + d)
+e <- rlang::get_expr(q)
+cdr <- rlang::node_cdr(e)
+car <- rlang::node_car(e)
+
+walk_cons(cdr)
+
+
+node_walk(car, print)
+
+
 q <- rlang::quo(a + b)
 head <- rlang::lang_head(q)
 tail <- rlang::lang_tail(q)
@@ -104,7 +196,28 @@ change_to_minus3 <- function(q) {
 	change_to_minus3(carn)
 }
 
+
+change_to_minus4 <- function(q) {
+	expr <- rlang::get_expr(q)
+	if(!rlang::is_lang(expr) & !rlang::is_node(expr)) return()
+	car <- rlang::node_car(expr)
+	if (car == rlang::sym("+")) {
+		rlang::mut_node_car(expr, rlang::sym("-"))
+	}
+	cdr <- rlang::node_cdr(expr)
+	carn <- rlang::node_car(cdr)
+	cdrn <- rlang::node_cdr(cdr)
+	for (index in 1:length(cdr))
+	{
+		change_to_minus4(carn[[index]])
+	}
+}
+
+
+
 q7 <- rlang::quo(a + (b * c) - d)
+change_to_minus4(q7)
+
 qtls_walk_outline  (q7)
 
 
@@ -112,8 +225,8 @@ a <- 2
 b <- 3
 c <- 4
 d <- 5
-q <- rlang::quo(a + (b + c) + d)
-change_to_minus3(q)
+q <- rlang::quo(a + (b * c) / d)
+#change_to_minus3(q)
 
 # this sequence traverses tree
 expr <- rlang::get_expr(q)
@@ -124,11 +237,13 @@ if (car == rlang::sym("+")) {
 }
 cdr <- rlang::node_cdr(expr)
 carn <- rlang::node_car(cdr)
+length(carn)
 cdrn <- rlang::node_cdr(cdr)
+length(cdrn)
 qtls_what_is_it(carn)
 qtls_what_is_it(cdrn)
 
-
+carn[[3]]
 
 expr <- rlang::get_expr(carn)
 rlang::is_lang(expr)
