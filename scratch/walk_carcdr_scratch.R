8e59b242497e1679749e770add24219777f3fa6a
dan87134 <dsullivan@danal.com>
2017-12-12 14:52:43 -0500

    Working example of qtls_walk_carcdr table
    Using expression column of table to modify quosure

--- scratch/walk_carcdr_scratch.R
+++ scratch/walk_carcdr_scratch.R
@@ -1,4 +1,20 @@
 # walk_carcdr scratch
+
+# example of ysing table produced by qtls_walk_carcdr
+# to modify quosure
 q <- rlang::quo(a * b + c * d)
 ast_tbl1 <- qtls_walk_carcdr(q)
+q
+
+car_tbl1 <- dplyr::filter(ast_tbl1, type == "car")
+car_tbl1$expression
+
+
+purrr::walk(car_tbl1$expression, function(expr) {
+	car <- rlang::node_car(expr)
+	if (rlang::is_symbol(car) & car == rlang::sym("+")) {
+		rlang::mut_node_car(expr, rlang::sym("-"))
+	}
+})
+
 q
