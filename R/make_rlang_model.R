486bed2043e6e64bef347204e52578c2846efc51
dan87134 <dsullivan@danal.com>
2017-12-20 16:40:20 -0500

    changes

--- R/make_rlang_model.R
+++ R/make_rlang_model.R
@@ -17,7 +17,7 @@ label_fix <- function(label) {
 
 
 
-qtls_make_rlang_table <- function(
+make_rlang_table <- function(
 	q_or_expr,
 	parent = 0L,
 	context = new.env() ,
@@ -39,7 +39,7 @@ qtls_make_rlang_table <- function(
 	#
 	# otherwise you can assume that it is an expression
 	if (rlang::is_quosure(q_or_expr)) {
-		e <- rlang::f_rhs(q_or_expr)
+		e <- rlang::get_expr(q_or_expr)
 	} else {
 		e <- q_or_expr
 	}
@@ -64,7 +64,7 @@ qtls_make_rlang_table <- function(
 	parent <- context$pass
 	context$pass <- context$pass + 1
 	if (rlang::is_lang(e)) {
-		car <- rlang::node_car(e)
+		car <- rlang::lang_head(e)
 		car_length <- length(car)
 		if (car_length == 1) {
 			qtls_make_rlang_table(car, order = 1,
@@ -80,7 +80,7 @@ qtls_make_rlang_table <- function(
 															source = "car")
 			}
 		}
-		cdr <- rlang::node_cdr(e)
+		cdr <- rlang::lang_tail(e)
 		cdr_length <- length(cdr)
 		if (cdr_length == 1) {
 			qtls_make_rlang_table(cdr, order = 1 + car_length,
@@ -100,3 +100,76 @@ qtls_make_rlang_table <- function(
 	}
 	tibble::rowid_to_column(context$tbl)
 	}
+
+
+
+qtls_make_rlang_table <- function(
+	q_or_expr,
+	parent = 0L,
+	context = new.env() ,
+	order = 1L,
+	source = "root") {
+	if (is.null(context$tbl)) {
+		context$tbl <-
+			tibble::tribble(
+			)
+		# pass is incremented after each row is added to tbl
+		context$pass <- 1L
+		context$expr_id <- 0L
+	}
+	# q might be a quosure or an expression
+	#
+	# If it is a quosure get its expression from
+	# its right hand side. Note that a quosure
+	# mimics a formula
+	#
+	# otherwise you can assume that it is an expression
+	# if (rlang::is_quosure(q_or_expr)) {
+	# 	e <- rlang::get_expr(q_or_expr)
+	# } else {
+	# 	e <- q_or_expr
+	# }
+	e <- q_or_expr
+	fcn_def <- inherits(e, "srcref")
+	dictionaryish <- rlang::is_dictionaryish(e)
+	context$tbl <- dplyr::bind_rows(context$tbl, tibble::tibble(
+		id = c(context$pass),
+		parent = c(parent),
+		#source = c(source),
+		address = c(pryr::address(e)),
+		expression = if (!fcn_def & !dictionaryish)  list(e) else  c(NA),
+		expr_type = c(typeof(e)),
+		expr_class = c(stringr::str_c(class(e), collapse = ", ")),
+		order = c(order),
+		expr_text = rlang::expr_text(e),
+		label =  if (!fcn_def) stringr::str_c(
+			label_fix(rlang::expr_text(e)), "\n", order,
+		 	collapse = "") else c(stringr::str_c("srcdef\n", order)),
+		what_is_expr = list(qtls_what_is_it(e))
+	)
+	)
+	parent <- context$pass
+	context$pass <- context$pass + 1
+	if (length(e) > 1) {
+		for(index in 1:length(e)) {
+			qtls_make_rlang_table(e[[index]], order = index,
+														context = context,
+														parent = parent,
+														source = "???")
+
+		}
+
+	}
+	tibble::rowid_to_column(context$tbl)
+}
+
+
+make_path <-function(tbl, current_id) {
+	parent <- dplyr::filter(tbl, current_id)
+	if (nrow(parent == 0)) {
+		return(current_id)
+	}
+	return (c(parent, make_path(tbl, parent)))
+}
+
+
